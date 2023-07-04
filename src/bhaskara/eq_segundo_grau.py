3ada1a7d160feb6a18c498d1dcb52202739f74b3
Ricardo Bafica Pontes <ricardo.bafica@gmail.com>
2023-07-04 09:39:41 -0300

    implementar opção de novo calculo ou fim de uso da funcao

--- src/bhaskara/eq_segundo_grau.py
+++ src/bhaskara/eq_segundo_grau.py
@@ -1,9 +1,5 @@
 # Este codigo encontra as raízes de uma equação de 2° grau: ax^2 + bx + c = 0
 
-# commit versão_0 "codigo que calcula raízes de uma eq 2o grau"
-# commit versão_1 "inclusao de condicao que exige coeficiente a <> 0"
-# commit versão_2 "uso da fc os.system() para limpar a tela do terminal"
-
 import math
 import os
 
@@ -29,19 +25,24 @@ def incognitas(a, b, c):
 
 def coleta_coeficientes():
 
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Este codigo encontra as raízes de uma equação de 2° grau:\n\
+    fim = True
+    while fim != 'sair':
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Este codigo encontra as raízes de uma equação de 2° grau:\n\
 ax^2 + bx + c = 0")
 
-    a = float(input("\nDigite o valor de a: "))
-    while a == 0:
-        print("Valor de a invalido, digite um valor para 'a' \
-diferente de zero: ")
         a = float(input("\nDigite o valor de a: "))
-    b = float(input("Digite o valor de b: "))
-    c = float(input("Digite o valor de c: "))
+        while a == 0:
+            print("Valor de a invalido, digite um valor para 'a' \
+diferente de zero: ")
+            a = float(input("\nDigite o valor de a: "))
+        b = float(input("Digite o valor de b: "))
+        c = float(input("Digite o valor de c: "))
 
-    incognitas(a, b, c)
+        incognitas(a, b, c)
 
+        fim = input("\nAcione 'enter' para continuar ou digite 'sair'\n")
 
+# chamada da funcao para teste de funcionamento apenas.
 # coleta_coeficientes()
